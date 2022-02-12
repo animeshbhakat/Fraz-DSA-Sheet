54331b5bbac8ba23c31d57cc4fe7eeaacbb6c464
animeshbhakat <bhakat.animesh1024@gmail.com>
2022-02-12 14:53:05 +0530

    added 11-container-with-most-water solution

---
+++ 11-container-with-most-water/twoPointerApproach.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int low=0, high  = height.length -1;
+        int maxArea =0;
+        while(low<high){
+            maxArea = Math.max(maxArea, Math.min(height[low], height[high]) *(high-low));
+        
+            if(height[low] < height[high]){
+                low++;
+            }
+            else{
+                high--;
+            }
+        }
+        return maxArea;
+        
+    }
+}
